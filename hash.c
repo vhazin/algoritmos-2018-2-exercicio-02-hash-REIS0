85d0945ada1c31b44d8987bd0bd75297de7491d5
Arthur O. Reis <aor@cesar.school>
2018-08-22 19:05:26 -0300

    rodando

--- hash.c
+++ hash.c
@@ -2,29 +2,29 @@
 
 int main(void) {
 
+  size_t i = 0;
   int count;
   unsigned long int a, b, x, n, c, d, m;
-  unsigned long int t, i;
-  scanf("%lu\n", &t);
+  unsigned long int t;
+  scanf("%lu", &t);
   int result[t];
 
   for (count = 0; count < t; count++){
 
-    scanf("%lu %lu %lu %lu %lu %lu %lu\n", &a, &b, &x, &n, &c, &d, &m);
+    scanf("%lu %lu %lu %lu %lu %lu %lu", &a, &b, &x, &n, &c, &d, &m);
     int h;
     int contador = 0;
 
-    for (size_t i = 0; i <= n; i++) {
+    for (i = 0; i <= n; i++) {
       h = (a * (x + i) + b) % m;
       if (c <= h && h <= d) {
           contador++;
       }
     }
     result[count] = contador;
-    count++;
   }
 
-  for (size_t i = 0; i < t; i++) {
+  for (i = 0; i < t; i++) {
     printf("%d\n", result[i]);
   }
   return 0;
